8d43cf52c775d32908edef84bec7f43375fd9f71
79064 <79064@MSI>
2021-11-29 00:28:54 +0300

    Добавьте файлы проекта.

---
+++ Lab11(bibl)/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11_bibl_
+{
+    public struct Equation
+    {
+        public int k;
+        public double b;
+
+        public int K
+        {
+            set
+            {
+                if (value == 0)
+                {
+                    Console.WriteLine("Недопустимое значание k!");
+                }
+                else
+                {
+                    k = value;
+                }
+            }
+            get
+            {
+                return k;
+            }
+        }
+        public Equation(int k, int b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public void Root()
+        {
+            double x = -b / k;
+            Console.Write("х={0}", x);
+        }
+    }
+}
